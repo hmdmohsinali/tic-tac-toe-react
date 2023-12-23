4f2bf83dd866c7a107984da4eb1ef775cfd765a1
hmdmohsinali <mohsin.201903372@gcuf.edu.pk>
2023-12-23 22:59:30 +0500

    Start of the project

---
+++ src/components/TicTacToe.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import circle from "../components/Assests/circle.png";
+import cross from "../components/Assests/cross.png";
+function TicTacToe() {
+  return (
+    <div>
+      <h1>Tic Tac Toe in React</h1>
+      <button className="">Reset</button>
+    </div>
+  )
+}
+
+export default TicTacToe
