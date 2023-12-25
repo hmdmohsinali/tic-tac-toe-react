6c104a1b13dc728677acd16a8698027cd0c61704
hmdmohsinali <mohsin.201903372@gcuf.edu.pk>
2023-12-25 13:49:08 +0500

    logic added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import TicTacToe from "./components/TicTacToe";
 
 function App() {
   return (
-    <div className="bg-[#0F172A] w-full h-screen"> 
+    <div className=" w-full h-screen"> 
       <TicTacToe/>
     </div>
   );
--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -3,9 +3,26 @@ import circle from "../components/Assests/circle.png";
 import cross from "../components/Assests/cross.png";
 function TicTacToe() {
   return (
-    <div>
-      <h1>Tic Tac Toe in React</h1>
-      <button className="">Reset</button>
+    <div className='text-center'>
+      <h1 className='mt-[50px] mb-6 text-white font-bold text-3xl text- flex justify-center items-center'>Tic Tac Toe in<span className='text-sky-300'> React</span></h1>
+      <div className='board'>
+        <div className='row1 '>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+        <div className='row2 '>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+        <div className='row3 '>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+      </div>
+      <button className="w-[100px] h-[50px] border-none cursor-pointer rounded-2xl bg-[#1f3540] text-xl text-sky-300 ">Reset</button>
     </div>
   )
 }
